5d9ae4d67d4de24e2d8f64183d57ee999996ade4
Utk45 <97669557+Utk45@users.noreply.github.com>
2023-10-21 22:55:50 +0530

    Add files via upload

--- p2.py
+++ p2.py
@@ -2,7 +2,8 @@ import socket
 import hashlib
 import time
 
-server_ip = '10.184.12.231'
+begin=time.time()
+server_ip = 'vayu.iitd.ac.in'
 server_port = 9801
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,12 +17,14 @@ miss=0
 offset = 0
 chunk_size = 1448
 burst=10
-gap=0.01
-gap2=0.01
+gap=0.02
+gap2=0.02
 num_packets=num_bytes//chunk_size
 packets=[b""]*num_packets
 visited=[False]*num_packets
 remaining=[]
+count1=0
+count2=0
 for i in range(num_packets):
     remaining.append(i)
 
@@ -34,61 +37,63 @@ while len(remaining)>0:
         for k in range(burst):
             request = f"Offset: {remaining[i*burst+k]*chunk_size}\nNumBytes: {chunk_size}\n\n"
             client_socket.sendto(request.encode(), (server_ip, server_port))
-
+        flag=False
         time.sleep(gap)
-        k=0
-        while k<burst:
-            client_socket.settimeout(gap2)
+        while True:
+            client_socket.settimeout(0.01)
             try:
                 response, server_address = client_socket.recvfrom(2000)
                 response_lines = response.decode().split('\n')
                 received_offset = int(response_lines[0].split()[1])
                 received_bytes = int(response_lines[1].split()[1])
-                data=b""
-                if response_lines[2]=="Squished":
-                    data = response_lines[4].encode()
-                    for x in range(5,len(response_lines)):
-                        data+="\n".encode()
-                        data+=response_lines[x].encode()
-                else:
-                    data = response_lines[3].encode()
-                    for x in range(4,len(response_lines)):
-                        data+="\n".encode()
-                        data+=response_lines[x].encode()
-                ind=remaining[i*burst+k]
-                if received_offset!= (ind)*chunk_size:
-                    ind_recv= received_offset//chunk_size
-                    if not visited[ind_recv]:
-                        packets[ind_recv]=data
-                        visited[ind_recv]=True
+                if(response.decode().count('Offset'))!=1:
+                    print(response.decode().count('Offset'))
+                ind_recv= received_offset//chunk_size
+                if not visited[ind_recv]:
+                    data=b""
+                    if response_lines[2]=="Squished":
+                        count1+=1
+                        data = response_lines[4].encode()
+                        for x in range(5,len(response_lines)):
+                            data+="\n".encode()
+                            data+=response_lines[x].encode()
+                        flag=True
                     else:
-                        miss+=1
-                    rem.append(ind)
-                else: 
-                    packets[ind]=data
-                    visited[ind]=True
-                k+=1
-
+                        data = response_lines[3].encode()
+                        for x in range(4,len(response_lines)):
+                            data+="\n".encode()
+                            data+=response_lines[x].encode()
+                    packets[ind_recv]=data
+                    visited[ind_recv]=True
             except socket.timeout:
+                count2+=1
+                #flag=True
                 break
-        while k<burst: 
-            rem.append(remaining[i*burst+k])
-            k+=1
-    for j in range(iterations*burst,len(remaining)):
-        rem.append(remaining[j])
-    remaining=[]
-    for q in range(len(rem)):
-        if not visited[rem[q]]:
-            remaining.append(rem[q])
+            if flag:
+                #print("hi")
+                flag=False
+                gap=0.05
+            else:
+                #print("hello")
+                gap=0.02
+
+    count2-=iterations
+
+    for j in range(len(remaining)):
+        if not visited[remaining[j]]:
+            rem.append(remaining[j])
+    remaining=rem
 
 received_data = b""
 for i in range(num_packets):
     received_data+=packets[i]
 
+time.sleep(0.1)
+
 while True and num_bytes%chunk_size!=0 :
     request = f"Offset: {num_packets*chunk_size}\nNumBytes: {num_bytes%chunk_size}\n\n"
     client_socket.sendto(request.encode(), (server_ip, server_port))
-    client_socket.settimeout(gap)
+    client_socket.settimeout(0.1)
     try:
         response, server_address = client_socket.recvfrom(2000)
         response_lines = response.decode().split('\n')
@@ -96,6 +101,7 @@ while True and num_bytes%chunk_size!=0 :
         received_bytes = int(response_lines[1].split()[1])
         data=b""
         if response_lines[2]=="Squished":
+            count1+=1
             data = response_lines[4].encode()
             for i in range(5,len(response_lines)):
                 data+="\n".encode()
@@ -112,14 +118,24 @@ while True and num_bytes%chunk_size!=0 :
         pass
         
 # print(received_data.decode())
+time.sleep(0.1)
 print(miss)
 md5_hash = hashlib.md5(received_data)
 md5_hex = md5_hash.hexdigest()
 
 submit_command = f"Submit: aseth@col334-672\nMD5: {md5_hex}\n\n"
 client_socket.sendto(submit_command.encode(), (server_ip, server_port))
-
-response, server_address = client_socket.recvfrom(1024)
-print("Validation Response:", response.decode())
-
+time.sleep(0.1)
+while True:
+    client_socket.settimeout(0.1)
+    try:
+        response, server_address = client_socket.recvfrom(2000)
+        if response.decode().count('Time') and response.decode().count('Penalty'):
+            print("Validation Response:", response.decode())
+    except socket.timeout:
+        client_socket.sendto(submit_command.encode(), (server_ip, server_port))
+        break
+print("Count1: ", count1)
+print("Count2: ", count2)
+print(time.time()-begin)
 client_socket.close()
