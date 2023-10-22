2da31cb1cabb94c7d740dd39fad0bca8abd14ad5
Hemang Sidana <97731299+HemangSidana@users.noreply.github.com>
2023-10-22 14:35:38 +0530

    Update p2_cur.py

--- p2_cur.py
+++ p2_cur.py
@@ -15,7 +15,7 @@ num_bytes = int(response.decode().split()[1])
 
 miss=0
 chunk_size = 1448
-gap=0.002
+gap=0.005
 num_packets=num_bytes//chunk_size
 packets=[b""]*num_packets
 visited=[False]*num_packets
@@ -27,8 +27,10 @@ for i in range(num_packets):
 
 while len(remaining)>0:
     i=0
+    flag=False
     while i<len(remaining):
         send= True
+        req_send=0
         while True:
             client_socket.settimeout(gap)
             try:
@@ -44,7 +46,9 @@ while len(remaining)>0:
                 if not visited[ind_recv]:
                     data=b""
                     if response_lines[2]=="Squished":
-                        # gap*=2
+                        # if(count1>100):
+                        #     print('Danger')
+                        #     print(gap)
                         count1+=1
                         data = response_lines[4].encode()
                         for x in range(5,len(response_lines)):
@@ -56,6 +60,10 @@ while len(remaining)>0:
                         for x in range(4,len(response_lines)):
                             data+="\n".encode()
                             data+=response_lines[x].encode()
+                        if i<len(remaining):
+                            request = f"Offset: {remaining[i]*chunk_size}\nNumBytes: {chunk_size}\n\n"
+                            client_socket.sendto(request.encode(), (server_ip, server_port))
+                            i+=1
                     packets[ind_recv]=data
                     visited[ind_recv]=True
                 send=False
@@ -68,71 +76,21 @@ while len(remaining)>0:
                     continue
                 request = f"Offset: {remaining[i]*chunk_size}\nNumBytes: {chunk_size}\n\n"
                 client_socket.sendto(request.encode(), (server_ip, server_port))
+                req_send+=1
                 i+=1
+                if req_send==9:
+                    send=False
     rem=[]
+    if flag:
+        gap*=1.5
+    else:
+        gap*=0.9
     for j in range(len(remaining)):
         if not visited[remaining[j]]:
             rem.append(remaining[j])
     remaining=rem
 
-
-
-# while len(remaining)>0:
-#     if len(remaining)<burst:
-#         burst=len(remaining)
-#     rem=[]
-#     iterations=len(remaining)//burst
-#     for i in range(iterations):
-#         for k in range(burst):
-#             request = f"Offset: {remaining[i*burst+k]*chunk_size}\nNumBytes: {chunk_size}\n\n"
-#             client_socket.sendto(request.encode(), (server_ip, server_port))
-#         flag=False
-#         time.sleep(gap)
-#         while True:
-#             client_socket.settimeout(0.01)
-#             try:
-#                 response, server_address = client_socket.recvfrom(2000)
-#                 response_lines = response.decode().split('\n')
-#                 received_offset = int(response_lines[0].split()[1])
-#                 received_bytes = int(response_lines[1].split()[1])
-#                 if(response.decode().count('Offset'))!=1:
-#                     print(response.decode().count('Offset'))
-#                 ind_recv= received_offset//chunk_size
-#                 if not visited[ind_recv]:
-#                     data=b""
-#                     if response_lines[2]=="Squished":
-#                         count1+=1
-#                         data = response_lines[4].encode()
-#                         for x in range(5,len(response_lines)):
-#                             data+="\n".encode()
-#                             data+=response_lines[x].encode()
-#                         flag=True
-#                     else:
-#                         data = response_lines[3].encode()
-#                         for x in range(4,len(response_lines)):
-#                             data+="\n".encode()
-#                             data+=response_lines[x].encode()
-#                     packets[ind_recv]=data
-#                     visited[ind_recv]=True
-#             except socket.timeout:
-#                 count2+=1
-#                 #flag=True
-#                 break
-#             if flag:
-#                 #print("hi")
-#                 flag=False
-#                 gap=0.05
-#             else:
-#                 #print("hello")
-#                 gap=0.02
-
-#     count2-=iterations
-
-#     for j in range(len(remaining)):
-#         if not visited[remaining[j]]:
-#             rem.append(remaining[j])
-#     remaining=rem
-
+print(gap)
 received_data = b""
 for i in range(num_packets):
     received_data+=packets[i]
