95d1ccd344f2290c2acad18949bdb98753dc6c81
Hemang Sidana <97731299+HemangSidana@users.noreply.github.com>
2023-10-25 21:47:23 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -2,7 +2,6 @@ import socket
 import hashlib
 import time
 
-begin=time.time()
 server_ip = 'vayu.iitd.ac.in'
 server_port = 9801
 
@@ -13,9 +12,8 @@ client_socket.sendto(send_size_command.encode(), (server_ip, server_port))
 response, server_address = client_socket.recvfrom(1024)
 num_bytes = int(response.decode().split()[1])
 
-miss=0
 chunk_size = 1448
-gap=0.01
+gap=0.005
 num_packets=num_bytes//chunk_size
 packets=[b""]*num_packets
 visited=[False]*num_packets
@@ -28,19 +26,15 @@ burst=10
 while len(remaining)>0:
     i=0
     while i<len(remaining):
-        if i%51==0:
-            print(i)
-        # print(i)
         for j in range(burst):
             if i>= len(remaining):
                 break
             request = f"Offset: {remaining[i]*chunk_size}\nNumBytes: {chunk_size}\n\n"
             client_socket.sendto(request.encode(), (server_ip, server_port))
             i+=1
-            time.sleep(0.006)
+            time.sleep(0.003)
         rtt=time.time()
-        # time.sleep(gap)
-        # cur=time.time()
+        end=-100
         got=0
         first=False
         while True:
@@ -61,7 +55,6 @@ while len(remaining)>0:
                         for x in range(5,len(response_lines)):
                             data+="\n".encode()
                             data+=response_lines[x].encode()
-
                     else:
                         data = response_lines[3].encode()
                         for x in range(4,len(response_lines)):
@@ -72,41 +65,31 @@ while len(remaining)>0:
                     visited[ind_recv]=True
                 got+=1
                 if not first:
-                    rtt-=time.time()
-                    # print(rtt)
+                    end=time.time()
                 first=True
             except socket.timeout:
-                # if not first:
-                #     continue
+                if not first and time.time()-rtt<0.1:
+                    continue
                 break
-        print(got,"got")
-        if got<8:
-            gap*=1.5
-        if got>9:
-            gap*=0.9
-        print(gap)
-        # if got*10<8*burst:
-        #     burst=burst//2
-        # else:
-        #     burst+=1
-        # print(burst)
+        rtt-=end
+        if rtt<=0 and got>7:
+            gap=-2*rtt        
+        else:
+            gap=0.005
     rem=[]
     for j in range(len(remaining)):
         if not visited[remaining[j]]:
             rem.append(remaining[j])
     remaining=rem
 
-print(gap)
 received_data = b""
 for i in range(num_packets):
     received_data+=packets[i]
 
-time.sleep(0.1)
-
 while num_bytes%chunk_size!=0 :
     request = f"Offset: {num_packets*chunk_size}\nNumBytes: {num_bytes%chunk_size}\n\n"
     client_socket.sendto(request.encode(), (server_ip, server_port))
-    client_socket.settimeout(0.1)
+    client_socket.settimeout(0.01)
     try:
         response, server_address = client_socket.recvfrom(2000)
         response_lines = response.decode().split('\n')
@@ -130,8 +113,6 @@ while num_bytes%chunk_size!=0 :
     except socket.timeout:
         pass
         
-# print(received_data.decode())
-print(miss)
 md5_hash = hashlib.md5(received_data)
 md5_hex = md5_hash.hexdigest()
 
@@ -146,7 +127,5 @@ while True:
     except socket.timeout:
         client_socket.sendto(submit_command.encode(), (server_ip, server_port))
         break
-print("Count1: ", count1)
-print("Count2: ", count2)
-print(time.time()-begin)
+
 client_socket.close()
